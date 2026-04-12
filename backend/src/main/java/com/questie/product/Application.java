71280fee36cd7a55dd46b214e9275862ac7508d5
ScorpQ <cansinlale01@gmail.com>
2026-04-12 23:24:23 +0300

    feat: new structure

---
+++ backend/src/main/java/com/questie/product/Application.java
@@ -0,0 +1,20 @@
+package com.questie.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	// https://www.wordbattletogether.com/
+	// https://www.wordbattletogether.com/
+
+	// http://localhost:8081/swagger-ui/index.html
+
+	// https://cdiptangshu.github.io/stomp-client/
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
